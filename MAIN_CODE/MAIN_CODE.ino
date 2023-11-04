1fa8544672df6964f839d64e4617b31b9195fd61
smkwon730 <44897282+smkwon730@users.noreply.github.com>
2023-11-04 19:53:42 +0900

    Update MAIN_CODE.ino

--- MAIN_CODE/MAIN_CODE.ino
+++ MAIN_CODE/MAIN_CODE.ino
@@ -19,32 +19,84 @@ void setup() {
 
 void loop() {
   int door = 0;
-  inout(door);
-  if (door = 1) { //외출
+  inout(door);                  // in FSR.h
+  
+  if (door = 1) {               //외출
     Serial.println("door=1");
     //Serial.println(door);
   }
-  if (door =2) {  //귀가
+
+  if (door =2) {                //귀가
     Serial.println("door=2");
     //Serial.println(door);
-    //delay(60000*5);
+
     int rfid = 0;
-    RFIDfunc();
-    
-    unsigned long currentMillis = millis(); //현재시간값 가져옴
-    if(currentMillis - previousMillis >= delayTime){ //1000초 시간이 흘렀는지 체크
-      previousMillis = currentMillis; //1초가 지나 참임으로 1초 지난 현재시간을 이전시간에 저장
-      
-      if (door = 2){
-        if (rfid = 1){
-          Serial.println("rfid=1");
-          //Serial.println(rfid);
-        }
-        if (rfid = 2) {
-          Serial.println("rfid=2");
-          //Serial.println(rfid);
-        }
+    unsigned long currentMillis = millis();
+
+    while (currentMillis - previousMillis <= delayTime) {   // 5분 제한시간
+
+      RFIDfunc(rfid);
+
+      if (rfid = 1) {               // 본인확인
+        Serial.println("rfid=1");
+        //Serial.println(rfid);     // 1 전송 (case1)
+        break;
       }
+
+      if (rfid = 2) {               // 본인확인 실패
+        Serial.println("rfid=2");
+        //Serial.println(rfid);     // 2 전송 (case2)
+        break;
+      }
+
+      previousMillis = currentMillis;
+    }
+
+    if (rfid = 0) {             // 5분 이후 미확인 == 본인확인 실패
+      rfid = 2;                 // 본인확인실패와 같은 값 전송
+      Serial.println("rfid=0"); // 테스트용
+      //Serial.println(rfid);   // 2 전송 (case2)
     }
   }
 }
+
+
+// void loop() {
+//   int door = 0;
+//   inout(door);                  // in FSR.h
+  
+//   if (door = 1) {               //외출
+//     Serial.println("door=1");
+//     //Serial.println(door);
+//   }
+//   if (door =2) {                //귀가
+//     Serial.println("door=2");
+//     //Serial.println(door);
+//     //delay(60000*5);
+    
+//     int check = 1;
+//     while (1)
+//     RFIDfunc();                   // in RFID.h
+
+//     if (rfid = 1) {               // 본인확인
+//       Serial.println("rfid=1");
+//       //Serial.println(rfid);
+//       int rfid = 0;
+//     }
+
+//     if (rfid = 2) {               // 본인확인 실패
+//       Serial.println("rfid=2");
+//       //Serial.println(rfid);
+//       int rfid = 0;
+//     }
+
+//     unsigned long currentMillis = millis(); // 현재시간값 가져옴
+//     if(currentMillis - previousMillis >= delayTime) { // 1000초 시간이 흘렀는지 체크
+//       previousMillis = currentMillis; // 1초가 지나 참임으로 1초 지난 현재시간을 이전시간에 저장
+//       if (rfid = 0) {             // 미확인
+//         Serial.println("rfid=0");
+//         //Serial.println(rfid);
+//       }
+//     }          
+//   }
+// }
