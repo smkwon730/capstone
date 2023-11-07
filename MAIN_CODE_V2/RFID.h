94fa65b6c2dcf332a65da419a7a9763fae4d4233
smkwon730 <44897282+smkwon730@users.noreply.github.com>
2023-11-07 23:30:29 +0900

    Update RFID.h

--- MAIN_CODE_V2/RFID.h
+++ MAIN_CODE_V2/RFID.h
@@ -7,8 +7,7 @@
 #define RST_PIN   9     // reset핀 설정
 #define SS_PIN    10    // 데이터를 주고받는 역할의 핀( SS = Slave Selector )
 
-extern MFRC522 mfrc522;           // 이 코드에서 MFR522를 이용하기 위해 mfrc객체를 생성해 줍니다.
-
+extern MFRC522 mfrc522; 
 const int LED = 8;                            // LED를 4번핀에 연결합니다.
 
 void RFIDsetup();
