7385ef1621a47ff781dc2dd7a8fe4c1222bd6ccb
smkwon730 <44897282+smkwon730@users.noreply.github.com>
2023-11-19 22:23:34 +0900

    Update InOut.cpp

--- MAIN_CODE_V2/InOut.cpp
+++ MAIN_CODE_V2/InOut.cpp
@@ -1,5 +1,14 @@
+// InOut.cpp
+
 #include "Arduino.h"
 #include "InOut.h"
+
+//-------------------------------------
+int sensorval_0;
+int sensorval_1;
+int sensorval_2;
+int sensorval_3;
+//-------------------------------------
 
 void InOutsetup() {
   pinMode(FSR0, INPUT);
@@ -11,37 +20,28 @@ void InOutsetup() {
 }
 
 int inout(void) {
-    //---------------------------------
-    long duration, distance;
+    
+    sensorval_0 = analogRead(A0);
+    sensorval_1 = analogRead(A1);
+    sensorval_2 = analogRead(A2);
+    sensorval_3 = analogRead(A3);
 
-    digitalWrite(TRIG, LOW);
-    delayMicroseconds(2);
-    digitalWrite(TRIG, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIG, LOW);
-
-    duration = pulseIn(ECHO, HIGH); // config trigger pin to output mode
-    distance = duration * 17 / 1000; // config echo pin to input mode
-
-    //-------------------------------------
-    int sensorval_0 = analogRead(A0);
-    int sensorval_1 = analogRead(A1);
-    int sensorval_2 = analogRead(A2);
-    int sensorval_3 = analogRead(A3);
-
-    //--------------------------------
+    static int val = 0;
 
     //int door = 0;
-    if (sensorval_3 < 50) {         // 문 여닫힘 감지. 문이 열렸을 경우
-        //delay(500);
+    //if (sensorval_3 < 50) {         // 문 여닫힘 감지. 문이 열렸을 경우
+    
+    if (sensorval_3 > 100) {         // 테스트용
+        //delay(100);
 
         if (sensorval_0 > 100 || sensorval_1 > 100 || sensorval_2 > 100) {  // 문 손잡이 압력 감지
-            //door = 1; 
-            return 1;                                                      // 외출
+            val = 1;                                            // 외출
         }
         else if (sensorval_0 < 100 && sensorval_1 < 100 && sensorval_2 < 100) {
-            //door = 2;   
-            return 2;                                                    // 귀가
+            val = 2;                                            // 귀가
         }
     }
+    Serial.print("inout: ");
+    Serial.println(val);  //test
+    return val;
 }
