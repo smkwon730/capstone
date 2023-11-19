7fbd97ca13a96dd8fcd938efbf3945e85d22034d
smkwon730 <44897282+smkwon730@users.noreply.github.com>
2023-11-19 22:23:10 +0900

    Update MAIN_CODE_V2.ino

--- MAIN_CODE_V2/MAIN_CODE_V2.ino
+++ MAIN_CODE_V2/MAIN_CODE_V2.ino
@@ -1,10 +1,12 @@
+// MAIN_CODE_V2.ino
 
 #include "InOut.h"
 #include "Clock.h"
 #include "RFID.h"
 
-unsigned long previous_time = 0;
-const long delay_time = 1000*60*5;   // 5 min
+//unsigned long previous_time = 0;
+//const long delay_time = 1000*60*5;   // 5 min
+const long delay_time = 1000*10;   // 30 sec (for test)
 
 
 void setup() {
@@ -15,38 +17,90 @@ void setup() {
 }
 
 void loop() {
-
-  unsigned long current_time = millis();
+  static int previousUart = 0;
   int uart = 0;
-  clockloop();
 
-  while (Alarm()) {    
-    if (inout() == 1)           // Didn't return home in time
-      Serial.println("out");
+  clockLoop();
+  int inoutResult = inout(); // inout 함수의 결과를 저장하여 불필요한 호출 방지
+
+  if (getAlarmState() == 1) {    
+
+    if (inoutResult == 1) {           // Didn't return home in time
+      //Serial.println("out");
       uart = 1;
-  }
-  
-  if (inout() == 2) {         // got home
-    Serial.println("in");
-
+    }
+    
+    if (inoutResult == 2) {         // got home
+      //Serial.println("in");
+    
     // 제한시간(5분) 이내 rfid 태그 코드
-    while(current_time - previous_time >= delay_time) {
-      previous_time = current_time;
-      
-      if (rfidCheck() == 1) {     // Authorized access
-        uart = 3;
-        break;
+      unsigned long start_time = millis();      
+      while(millis() - start_time < delay_time) {
+        
+        int rfidResult = rfidCheck();
+        
+        if (rfidResult == 1) {     // Authorized access
+          uart = 3;
+          break;
+        }
+        if (rfidResult == 2) {     // Access denied
+          uart = 2;
+          break;
+        }
+        // else {                      // no access in 5min
+        //   uart = 2;
+        // }
       }
-      if (rfidCheck() == 2) {     // Access denied
-        uart = 2;
-        break;
-      }
-      else {                      // no access in 5min
+      if (uart == 0) {
         uart = 2;
       }
     }
   }
-  if (uart != 0) Serial.println(uart);
-  
-  delay(100);
+  /* uart가 0이 아니면서 이전의 uart와 다르다면, 그리고 uart가 0이 아니라면
+  uart를 출력하고 previousUart를 업데이트한다. */
+  if (uart != previousUart && uart != 0) {
+    Serial.print("uart: ");
+    Serial.println(uart);
+    previousUart = uart;
+  } else if (uart != previousUart && uart == 0) {
+    previousUart = 0;
+  }
+
+  //delay(100);
 }
+
+
+
+
+
+
+
+  /*//======= inout() test =========//
+  inout();
+  Serial.println(inout());
+  //======= Alarm() test =========//
+  while (Alarm()) {
+    Serial.println("alarm: 1");
+  }
+  Serial.println("alarm: 0");
+  //====== rfidCheck() test ======//
+  unsigned long current_time = millis();
+
+  while(current_time - previous_time >= delay_time) {
+    Serial.println(current_time);
+    previous_time = current_time;
+
+    if (rfidCheck() == 1) {     // Authorized access
+      Serial.println("Authorized access");
+      break;
+    }
+    if (rfidCheck() == 2) {     // Access denied
+      Serial.println("Access denied");
+      break;
+    }
+  }
+  if (rfidCheck() != 1 || rfidCheck() != 2) {                      // no access in 5 min
+    if (current_time - previous_time >= delay_time) {
+      Serial.println("no access in 5 min");
+    }
+  }*/
