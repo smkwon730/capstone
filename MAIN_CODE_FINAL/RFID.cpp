5bf090c4462f4a612c34e9d035206cde3a8a7025
smkwon730 <44897282+smkwon730@users.noreply.github.com>
2023-12-05 01:23:42 +0900

    Update RFID.cpp

--- MAIN_CODE_FINAL/RFID.cpp
+++ MAIN_CODE_FINAL/RFID.cpp
@@ -3,16 +3,15 @@
 #include "Arduino.h"
 #include "RFID.h"
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // RFID 모듈 핀 지정
 
 void RFIDsetup() {
-  SPI.begin();          // SPI 초기화(SPI : 하나의 마스터와 다수의 SLAVE(종속적인 역활)간의 통신 방식)
+  SPI.begin();          // SPI 초기화 (SPI : 하나의 마스터와 다수의 SLAVE(종속적인 역활)간의 통신 방식)
   mfrc522.PCD_Init();   // Initiate MFRC522
-  pinMode(LED, OUTPUT); // 4번핀을 출력으로 설정
-}
+  pinMode(LED, OUTPUT);
 
 int rfidCheck(void) {
-        if (  mfrc522.PICC_IsNewCardPresent() &&  mfrc522.PICC_ReadCardSerial() ){
+        if (  mfrc522.PICC_IsNewCardPresent() &&  mfrc522.PICC_ReadCardSerial() ){  // RFID가 태그된 경우 RFID의 UID를 읽어옴
           String content= "";
           byte letter;
           for (byte i = 0; i < mfrc522.uid.size; i++) {
@@ -21,12 +20,12 @@ int rfidCheck(void) {
           }
           content.toUpperCase(); 
         
-          if (content.substring(1) == "10 2C D6 20") { //change here the UID of the card/cards that you want to give access
-              return 1;
+          if (content.substring(1) == "10 2C D6 20") {       // 정상적인 RFID가 태그되었을 경우
+              return 1;                                      // 1 반환
           }
-          else if (content.substring(1) != "10 2C D6 20") {
-              return 2;
+          else if (content.substring(1) != "10 2C D6 20") {  // 사용자의 RFID UID가 아닐 경우
+              return 2;                                      // 2 반환
           }  
     }
-  return 0;
+  return 0;                                                  // RFID가 태그되지 않았을 경우 0 반환
 }
